99666a308e67a3b5e9b65e75a5e2d10762532aaf
macedoogui <macedoogui@gmail.com>
2022-04-27 23:00:42 -0300

    logo e icones

--- cadastro-venture-labs/src/Home.jsx
+++ cadastro-venture-labs/src/Home.jsx
@@ -1,9 +1,29 @@
 import "./Home.css";
 import PessoaLista from "./PessoaLista";
+import logo from "./assets/logo.webp"
+import list from "./assets/list.svg"
 
 function Home() {
   return (
     <div className="Home">
+
+      <div className="Header">
+
+        <div className="Header__Logo">
+          <img src={logo} width="70px" alt="Logo VentureLabs"></img>
+        </div>
+
+        <div className="titulo">VentureLabs
+        </div>
+
+        <div className="Header__icone">
+          <img src={list}
+          width="30px" alt="Ícone menu"></img>
+        </div>
+
+      </div>
+      
+      
       <div className="Home__container">
         <PessoaLista />
       </div>
